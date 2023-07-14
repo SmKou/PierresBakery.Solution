fa0b9f752804f5c98311cc24aa69c11f4f196aa9
SmKou <66178856+SmKou@users.noreply.github.com>
2023-07-14 10:29:35 -0700

    initfail bread pastry GetSubtotal

--- PierresBakery.Tests/ModelTests/MenuItem.Tests.cs
+++ PierresBakery.Tests/ModelTests/MenuItem.Tests.cs
@@ -16,12 +16,37 @@ namespace PierresBakery.Tests
         }
 
         [TestMethod]
+        [DataRow(1, 5)]
+        [DataRow(2, 10)]
+        [DataRow(3, 10)]
+        [DataRow(4, 15)]
+        public void bread_ReturnSubtotalOfBread_Int(int amt, int total)
+        {
+            MenuItem bread = new Bread();
+            int result = bread.GetSubtotal(amt);
+            Assert.AreEqual(total, result);
+        }
+
+        [TestMethod]
         public void pastry_ConstructMenuItem_Void()
         {
             MenuItem pastry = new Pastry();
             Assert.AreEqual("pastry", pastry.ItemName);
             Assert.AreEqual(2, pastry.Cost);
             Assert.AreEqual(3, pastry.DealAmount);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2)]
+        [DataRow(2, 4)]
+        [DataRow(3, 6)]
+        [DataRow(4, 6)]
+        [DataRow(5, 8)]
+        public void pastry_ReturnSubtotalOfBread_Int(int amt, int total)
+        {
+            MenuItem pastry = new Pastry();
+            int result = pastry.GetSubtotal(amt);
+            Assert.AreEqual(total, result);
         }
     }
 }
